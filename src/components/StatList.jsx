5cfd89af19b6e5e4860070c0cd5e480533d07d70
Kowshik Kuri <ratulkuri@gmail.com>
2023-09-30 17:32:11 +0600

    Stats design Update

--- src/components/StatList.jsx
+++ src/components/StatList.jsx
@@ -12,42 +12,50 @@ const StatList = async () => {
           <div className="flex flex-wrap">
             {
               StatList?.followers &&
-              <div className="mb-8 w-1/2 md:w-1/2 lg:w-1/4 text-center">
-                <span className="mb-6 inline-block p-4 rounded-lg bg-green-100">
-                  <UserGroupIcon className="w-[30px] h-[30px] text-black" />
-                </span>
-                <h3 className="text-2xl font-bold">{StatList?.followers}</h3>
-                <p className="text-gray-500">Followers</p>
+              <div className="mb-8 lg:mb-0 w-1/2 md:w-1/2 lg:w-1/4 text-center p-4">
+                <div className="font-poppin rounded-3xl py-12 theme-shadow">
+                  <span className="mb-6 inline-block p-8 rounded-lg bg-green-100">
+                    <UserGroupIcon className="w-[30px] h-[30px] text-black" />
+                  </span>
+                  <h3 className="text-3xl font-bold mb-2">{StatList?.followers}</h3>
+                  <p className="text-black font-medium">Followers</p>
+                </div>
               </div>
             }
             {
               StatList?.solved &&
-              <div className="mb-8 w-1/2 md:w-1/2 lg:w-1/4 text-center">
-                <span className="mb-6 inline-block p-4 rounded-lg bg-green-100">
-                  <HandThumbUpIcon className="w-[30px] h-[30px] text-black" />
-                </span>
-                <h3 className="text-2xl font-bold">{StatList?.solved}</h3>
-                <p className="text-gray-500">Solved Problems</p>
+              <div className="mb-8 lg:mb-0 w-1/2 md:w-1/2 lg:w-1/4 text-center p-4">
+                <div className="font-poppin rounded-3xl py-12 theme-shadow">
+                  <span className="mb-6 inline-block p-8 rounded-lg bg-green-100">
+                    <HandThumbUpIcon className="w-[30px] h-[30px] text-black" />
+                  </span>
+                  <h3 className="text-3xl font-bold mb-2">{StatList?.solved}</h3>
+                  <p className="text-black font-medium">Solved Problems</p>
+                </div>
               </div>
             }
             {
               StatList?.customers &&
-              <div className="mb-8 w-1/2 md:w-1/2 lg:w-1/4 text-center">
-                <span className="mb-6 inline-block p-4 rounded-lg bg-green-100">
-                  <FaceSmileIcon className="w-[30px] h-[30px] text-black" />
-                </span>
-                <h3 className="text-2xl font-bold">{StatList?.customers}</h3>
-                <p className="text-gray-500">Happy Customers</p>
+              <div className="mb-8 lg:mb-0 w-1/2 md:w-1/2 lg:w-1/4 text-center p-4">
+                <div className="font-poppin rounded-3xl py-12 theme-shadow">
+                  <span className="mb-6 inline-block p-8 rounded-lg bg-green-100">
+                    <FaceSmileIcon className="w-[30px] h-[30px] text-black" />
+                  </span>
+                  <h3 className="text-3xl font-bold mb-2">{StatList?.customers}</h3>
+                  <p className="text-black font-medium">Happy Customers</p>
+                </div>
               </div>
             }
             {
               StatList?.projects &&
-              <div className="mb-8 w-1/2 md:w-1/2 lg:w-1/4 text-center">
-                <span className="mb-6 inline-block p-4 rounded-lg bg-green-100">
-                  <Squares2X2Icon className="w-[30px] h-[30px] text-black" />
-                </span>
-                <h3 className="text-2xl font-bold">{StatList?.projects}</h3>
-                <p className="text-gray-500">Projects</p>
+              <div className="mb-8 lg:mb-0 w-1/2 md:w-1/2 lg:w-1/4 text-center p-4">
+                <div className="font-poppin rounded-3xl py-12 theme-shadow">
+                  <span className="mb-6 inline-block p-8 rounded-lg bg-green-100">
+                    <Squares2X2Icon className="w-[30px] h-[30px] text-black" />
+                  </span>
+                  <h3 className="text-3xl font-bold mb-2">{StatList?.projects}</h3>
+                  <p className="text-black font-medium">Projects</p>
+                </div>
               </div>
             }
           </div>
