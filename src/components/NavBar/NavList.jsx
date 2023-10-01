eaf2511bfae14bfea87ef515c9087a303da75550
Kowshik Kuri <ratulkuri@gmail.com>
2023-10-01 10:08:27 +0600

    Add active status in primary nav

--- src/components/NavBar/NavList.jsx
+++ src/components/NavBar/NavList.jsx
@@ -1,63 +1,63 @@
+"use client"
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { Typography } from '../MaterialReact';
 
 const NavList = () => {
-  return (
-    <>
-        <ul className="primary-nav-list mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-medium font-poppin"
-            >
-                <Link href="/" className="nav-link flex items-center">
-                    Home
-                </Link>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-medium font-poppin"
-            >
-                <Link href="/team" className="nav-link flex items-center">
-                    Team
-                </Link>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-medium font-poppin"
-            >
-                <Link href="/services" className="nav-link flex items-center">
-                    Services
-                </Link>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-medium font-poppin"
-            >
-                <Link href="/projects" className="nav-link flex items-center">
-                    Projects
-                </Link>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-medium font-poppin"
-            >
-                <Link href="/testimonials" className="nav-link flex items-center">
-                    Testimonials
-                </Link>
-            </Typography>
-        </ul>
-    </>
-  )
+    const pathname = usePathname()
+
+    return (
+        <>
+            <ul className="primary-nav-list mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+                <Typography
+                    as="li"
+                    variant="small"
+                    className="p-1 font-medium font-poppin"
+                >
+                    <Link href="/" className={`${pathname === "/" ? "text-theme-green" : "text-gray-800"} nav-link flex items-center`}>
+                        Home
+                    </Link>
+                </Typography>
+                <Typography
+                    as="li"
+                    variant="small"
+                    className="p-1 font-medium font-poppin"
+                >
+                    <Link href="/team" className={`${pathname === "/team" ? "text-theme-green" : "text-gray-800"} nav-link flex items-center`}>
+                        Team
+                    </Link>
+                </Typography>
+                <Typography
+                    as="li"
+                    variant="small"
+                    className="p-1 font-medium font-poppin"
+                >
+                    <Link href="/services" className={`${pathname === "/services" ? "text-theme-green" : "text-gray-800"} nav-link flex items-center`}>
+                        Services
+                    </Link>
+                </Typography>
+                <Typography
+                    as="li"
+                    variant="small"
+                    className="p-1 font-medium font-poppin"
+                >
+                    <Link href="/projects" className={`${pathname === "/projects" ? "text-theme-green" : "text-gray-800"} nav-link flex items-center`}>
+                        Projects
+                    </Link>
+                </Typography>
+                <Typography
+                    as="li"
+                    variant="small"
+                    className="p-1 font-medium font-poppin"
+                >
+                    <Link href="/testimonials" className={`${pathname === "/testimonials" ? "text-theme-green" : "text-gray-800"} nav-link flex items-center`}>
+                        Testimonials
+                    </Link>
+                </Typography>
+            </ul>
+        </>
+    )
 }
 
 export default NavList;
